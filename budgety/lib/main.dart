4ebbeea4db5a516a103a55f19e21f2072c2d9a07
Dawid Sibiński <dawidsibinski@gmail.com>
2020-11-29 19:52:22 +0100

    Firebase integration added

--- budgety/lib/main.dart
+++ budgety/lib/main.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
-  runApp(BudgetyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return UnexpectedError();
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return BudgetyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Loading();
+      },
+    );
+  }
 }
 
 class BudgetyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,5 +57,39 @@ class BudgetyApp extends StatelessWidget {
 
   void buttonClicked() {
     print("Button clicked func");
+  }
+}
+
+class UnexpectedError extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primaryColor: Colors.lightGreen,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Budgety'),
+            ),
+            body: Center(
+              child: Text('Something went wrong... try again later'),
+            )));
+  }
+}
+
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primaryColor: Colors.lightGreen,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Budgety'),
+            ),
+            body: Center(
+              child: Text('App loading...'),
+            )));
   }
 }
