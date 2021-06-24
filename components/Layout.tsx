2d08c085cb8144f7601a8174c365eb8e709d6ff6
Dawid Sibiński <dawidsibinski@gmail.com>
2021-06-24 10:47:13 +0800

    Got rid of scrollbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ children }: any) => {
 		<>
 			<Meta />
 			<Navigation />
-			<div className="flex flex-col items-center justify-center h-screen">
+			<div className="flex flex-col mt-2 items-center justify-center h-full">
 				<main className="flex flex-1 flex-col items-center justify-center space-y-4">
 					<Header />
 					{children}
