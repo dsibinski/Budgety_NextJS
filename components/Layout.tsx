e3de60aaddc5072c02d660d28c5f7622f550ec00
Dawid Sibiński <dawidsibinski@gmail.com>
2021-06-24 10:17:09 +0800

    Fully migrated to TainwindCSS
    Also added icons and new navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,6 @@
 import Navigation from './Navigation';
 import Meta from './Meta';
 import Header from './Header';
-import styles from '../styles/Layout.module.css';
 
 const Layout = ({ children }: any) => {
 	return (
