7de85432effab287e3e3f575d10cf7485386e865
Dawid Sibiński <dawidsibinski@gmail.com>
2021-08-20 12:47:08 +0800

    Adding categories implemented
    * TODO1: what about edition? Maybe add validation on existing categories
    * TODO2: add a button/link to return to categories list

--- pages/categories/add.tsx
+++ pages/categories/add.tsx
@@ -1,5 +1,129 @@
+import {
+	Flex,
+	Input,
+	Button,
+	FormControl,
+	FormLabel,
+	FormErrorMessage,
+	RadioGroup,
+	HStack,
+	Radio,
+} from '@chakra-ui/react';
+import { Formik, Form, Field, FieldProps } from 'formik';
+import OperationType from '../../models/operationType';
+import firebase from 'firebase';
+import { useAuthUser } from 'next-firebase-auth';
+
 const Add = () => {
-	return <div>Hi</div>;
+	const AuthUser = useAuthUser();
+	function validateName(value: string) {
+		let error;
+		if (!value) {
+			error = 'Category name is required';
+		}
+		return error;
+	}
+
+	// function validateType(value: OperationType) {
+	// 	let error;
+	// 	if (!value) {
+	// 		error = 'Category type is required';
+	// 	}
+	// 	return error;
+	// }
+
+	return (
+		<Flex direction="column">
+			<Formik
+				initialValues={{ name: '', type: OperationType.Expense }}
+				onSubmit={(values, actions) => {
+					firebase
+						.firestore()
+						.collection('users')
+						.doc(AuthUser.id as string)
+						.collection('categories')
+						.add(values)
+						.then((result) => {
+							alert('Added successfully');
+							actions.setSubmitting(false);
+						});
+				}}
+			>
+				{({ isSubmitting }) => (
+					<Form>
+						<Field name="name" validate={validateName}>
+							{({ field, form }: any) => (
+								<FormControl
+									isInvalid={
+										form.errors.name && form.touched.name
+									}
+								>
+									<FormLabel htmlFor="name">
+										Category name
+									</FormLabel>
+									<Input
+										{...field}
+										id="name"
+										placeholder="Name"
+									/>
+									<FormErrorMessage>
+										{form.errors.name}
+									</FormErrorMessage>
+								</FormControl>
+							)}
+						</Field>
+						<Field name="type">
+							{({ field, form }: FieldProps) => {
+								const { onChange, ...rest } = field;
+								return (
+									<FormControl
+										id="type"
+										isInvalid={
+											!!form.errors['type'] &&
+											!!form.touched['type']
+										}
+									>
+										<FormLabel htmlFor={'type'}>
+											Category type
+										</FormLabel>
+										<RadioGroup {...rest} id="type">
+											<HStack spacing="24px">
+												<Radio
+													onChange={onChange}
+													value={
+														OperationType.Expense
+													}
+												>
+													{OperationType.Expense}
+												</Radio>
+												<Radio
+													onChange={onChange}
+													value={OperationType.Income}
+												>
+													{OperationType.Income}
+												</Radio>
+											</HStack>
+										</RadioGroup>
+										<FormErrorMessage>
+											{form.errors['type']}
+										</FormErrorMessage>
+									</FormControl>
+								);
+							}}
+						</Field>
+						<Button
+							mt={4}
+							colorScheme="teal"
+							isLoading={isSubmitting}
+							type="submit"
+						>
+							Save
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</Flex>
+	);
 };
 
 export default Add;
