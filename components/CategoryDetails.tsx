0817837df2148ef7d6cc6b53580053746051b59e
Dawid Sibiński <dawidsibinski@gmail.com>
2021-10-09 11:15:28 +0800

    CategoryDetails component extracted

---
+++ components/CategoryDetails.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import {
+	Input,
+	Button,
+	FormControl,
+	FormLabel,
+	FormErrorMessage,
+	RadioGroup,
+	HStack,
+	Radio,
+} from '@chakra-ui/react';
+import { Formik, Form, Field, FieldProps } from 'formik';
+import OperationType from 'models/operationType';
+import Category from 'models/category';
+
+type CategoryDetailsProps = {
+	category: Category;
+	onSubmit: (value: Category) => Promise<void>;
+};
+const CategoryDetails = (props: CategoryDetailsProps) => {
+	const validateName = function (value: string) {
+		let error;
+		if (!value) {
+			error = 'Category name is required';
+		}
+		return error;
+	};
+
+	return (
+		<div>
+			<Formik initialValues={props.category} onSubmit={props.onSubmit}>
+				{({ isSubmitting }) => (
+					<Form>
+						<Field name="name" validate={validateName}>
+							{({ field, form }: FieldProps) => (
+								<FormControl
+									isInvalid={
+										!!form.errors['name'] &&
+										!!form.touched['name']
+									}
+								>
+									<FormLabel htmlFor="name">
+										Category name
+									</FormLabel>
+									<Input
+										{...field}
+										id="name"
+										placeholder="Name"
+									/>
+									<FormErrorMessage>
+										{form.errors.name}
+									</FormErrorMessage>
+								</FormControl>
+							)}
+						</Field>
+						<Field name="type">
+							{({ field, form }: FieldProps) => {
+								const { onChange, ...rest } = field;
+								return (
+									<FormControl
+										id="type"
+										isInvalid={
+											!!form.errors['type'] &&
+											!!form.touched['type']
+										}
+									>
+										<FormLabel htmlFor={'type'}>
+											Category type
+										</FormLabel>
+										<RadioGroup {...rest} id="type">
+											<HStack spacing="24px">
+												<Radio
+													onChange={onChange}
+													value={
+														OperationType.Expense
+													}
+												>
+													{OperationType.Expense}
+												</Radio>
+												<Radio
+													onChange={onChange}
+													value={OperationType.Income}
+												>
+													{OperationType.Income}
+												</Radio>
+											</HStack>
+										</RadioGroup>
+										<FormErrorMessage>
+											{form.errors['type']}
+										</FormErrorMessage>
+									</FormControl>
+								);
+							}}
+						</Field>
+						<Button
+							mt={4}
+							colorScheme="teal"
+							isLoading={isSubmitting}
+							type="submit"
+						>
+							Save
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</div>
+	);
+};
+
+export default CategoryDetails;
--- pages/categories/add.tsx
+++ pages/categories/add.tsx
@@ -1,121 +1,38 @@
-import {
-	Flex,
-	Input,
-	Button,
-	FormControl,
-	FormLabel,
-	FormErrorMessage,
-	RadioGroup,
-	HStack,
-	Radio,
-} from '@chakra-ui/react';
-import { Formik, Form, Field, FieldProps } from 'formik';
-import OperationType from '../../models/operationType';
-import firebase from 'firebase';
+import { Flex } from '@chakra-ui/react';
 import { AuthAction, useAuthUser, withAuthUser } from 'next-firebase-auth';
 import { useRouter } from 'next/dist/client/router';
+import CategoryDetails from 'components/CategoryDetails';
+import Category from 'models/category';
+import OperationType from 'models/operationType';
+import firebase from 'firebase';
 
 const Add = () => {
 	const router = useRouter();
 	const AuthUser = useAuthUser();
-	function validateName(value: string) {
-		let error;
-		if (!value) {
-			error = 'Category name is required';
+	let newCategory: Category = {
+		id: '',
+		name: '',
+		type: OperationType.Expense,
+	};
+
+	const onAddCategory = async function (value: Category) {
+		try {
+			await firebase
+				.firestore()
+				.collection('users')
+				.doc(AuthUser.id as string)
+				.collection('categories')
+				.add(value);
+		} catch (error) {
+			alert('There was an error when adding new category.');
 		}
-		return error;
-	}
+
+		router.push('/categories');
+	};
 
 	return (
 		<Flex direction="column">
-			<Formik
-				initialValues={{ name: '', type: OperationType.Expense }}
-				onSubmit={(values, actions) => {
-					firebase
-						.firestore()
-						.collection('users')
-						.doc(AuthUser.id as string)
-						.collection('categories')
-						.add(values)
-						.then((result) => {
-							actions.setSubmitting(false);
-							router.push('/categories');
-						});
-				}}
-			>
-				{({ isSubmitting }) => (
-					<Form>
-						<Field name="name" validate={validateName}>
-							{({ field, form }: any) => (
-								<FormControl
-									isInvalid={
-										form.errors.name && form.touched.name
-									}
-								>
-									<FormLabel htmlFor="name">
-										Category name
-									</FormLabel>
-									<Input
-										{...field}
-										id="name"
-										placeholder="Name"
-									/>
-									<FormErrorMessage>
-										{form.errors.name}
-									</FormErrorMessage>
-								</FormControl>
-							)}
-						</Field>
-						<Field name="type">
-							{({ field, form }: FieldProps) => {
-								const { onChange, ...rest } = field;
-								return (
-									<FormControl
-										id="type"
-										isInvalid={
-											!!form.errors['type'] &&
-											!!form.touched['type']
-										}
-									>
-										<FormLabel htmlFor={'type'}>
-											Category type
-										</FormLabel>
-										<RadioGroup {...rest} id="type">
-											<HStack spacing="24px">
-												<Radio
-													onChange={onChange}
-													value={
-														OperationType.Expense
-													}
-												>
-													{OperationType.Expense}
-												</Radio>
-												<Radio
-													onChange={onChange}
-													value={OperationType.Income}
-												>
-													{OperationType.Income}
-												</Radio>
-											</HStack>
-										</RadioGroup>
-										<FormErrorMessage>
-											{form.errors['type']}
-										</FormErrorMessage>
-									</FormControl>
-								);
-							}}
-						</Field>
-						<Button
-							mt={4}
-							colorScheme="teal"
-							isLoading={isSubmitting}
-							type="submit"
-						>
-							Save
-						</Button>
-					</Form>
-				)}
-			</Formik>
+			<CategoryDetails category={newCategory} onSubmit={onAddCategory} />
 		</Flex>
 	);
 };
